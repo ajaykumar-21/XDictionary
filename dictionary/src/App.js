a96f1085b71024e41a597e1184dcdd131a6b4c3f
ajaykumar-21 <ajaykumarpk789@gmail.com>
2024-02-27 18:02:08 +0530

    make changes in dic statwe

--- dictionary/src/App.js
+++ dictionary/src/App.js
@@ -1,15 +1,15 @@
 import React, { useState } from "react";
 
-const dictionary = [
-  {
-    word: "React",
-    meaning: "A JavaScript library for building user interfaces.",
-  },
-  { word: "Component", meaning: "A reusable building block in React." },
-  { word: "State", meaning: "An object that stores data for a component." },
-];
-
 function App() {
+  const [dictionary] = useState([
+    {
+      word: "React",
+      meaning: "A JavaScript library for building user interfaces.",
+    },
+    { word: "Component", meaning: "A reusable building block in React." },
+    { word: "State", meaning: "An object that stores data for a component." },
+  ]);
+
   const [searchTerm, setSearchTerm] = useState("");
   const [searchResult, setSearchResult] = useState("");
 
@@ -27,7 +27,7 @@ function App() {
 
   return (
     <div>
-      <h1>Dictionary App</h1>
+      <h1>XDictionary</h1>
       <div>
         <input
           type="text"
